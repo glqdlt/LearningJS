864e978b9228d81ef0f2f16cbb300355a51dd658
glqdlt <dlfdnd0725@gmail.com>
2017-11-25 21:22:43 +0900

    page 214 휴식

--- src/chap004/es6/scope.js
+++ src/chap004/es6/scope.js
@@ -39,3 +39,59 @@ let jhun = {
 
 
 //194 page 휴식
+
+{
+    // external block
+    let x = {color:"blue"};
+    let y = x;
+    let z = 3;
+    {
+        // inner block
+        let x = 5;
+        console.log(x); // inner x
+        console.log(y.color); // external x
+        y.color="red";
+        console.log(z);
+    }
+    console.log(x.color);
+    console.log(y.color);
+    console.log(z);
+}
+
+// 클로져
+
+let globalFunc;
+
+
+// globalFunc 와  blockVar 가 선언된 inner block 은 완전 다른 차원의 존재인데, inner block 에서 globalFunc에 붙게 되면서 접근을 할 수 있음,
+// 블록 스코프 안에서 글로벌 함수 또는 외부 함수를 정의하면 해당 스코프의 관계는 좁아(클로징) 된다고 해서 클로져임
+
+
+{
+    let blockVar = 'inner var';
+    globalFunc = function(){
+        return blockVar;
+    }
+}
+
+console.log(globalFunc());
+
+
+let fff;
+
+{
+    let o = {note: 'safe'};
+    fff = function(){
+        return o;
+    }
+
+}
+
+
+
+console.log(fff().note);
+let oRef = fff();
+
+oRef.note = 'not safe';
+
+console.log(oRef);
