7118bf91fe7cb9a2bfd8c1db792441b6655c3246
iw.jhun <dlfdnd0725@gmail.com>
2017-11-28 18:26:14 +0900

    chapter018

--- src/chap014/Events.js
+++ src/chap014/Events.js
@@ -2,39 +2,46 @@ const EventEmitter = require('events').EventEmitter;
 
 class countdown extends EventEmitter {
 
-    constructor(sec, supers) {
+    constructor(sec, superstitious) {
         super();
         this.seconds = sec;
-        this.supersittious = !!supers;
+        this.superstitious = !!superstitious;
     }
 
     go() {
         const countdown = this;
+        const timeoutIds = [];
         return new Promise(function (resolve, reject) {
 
             for (let i = countdown.seconds; i >= 0; i--) {
-                setTimeout(function () {
-                    if (countdown.supersittious && i === 13) {
+                timeoutIds.push(setTimeout(function () {
+                    
+                    // 13일 때
+                    if (countdown.superstitious && i === 13){
+                        // 다 취소하고, 에러로 넘김.
+                        timeoutIds.forEach(clearTimeout);
                         return reject(new Error("oh my goodddd"));
                     }
-                    countdown.emit("tick", i);
-                    if (i === 0) resolve();
-                }, (countdown.seconds - i) * 1000);
+                    countdown.emit('tick',i);
+                    if(i===0){
+                        resolve();
+                    }
+                }, (countdown.seconds -i)*1000))
             }
         });
     };
 };
 
 
-const c = new countdown(15,true);
-c.on('tick',function(i){
-    if(i>0) console.log(i + '...');
+const c = new countdown(15, true);
+c.on('tick', function (i) {
+    if (i > 0) console.log(i + '...');
 });
 c.go()
-    .then(function(){
+    .then(function () {
         console.log("go!");
     })
-    .catch(function(err){
+    .catch(function (err) {
         console.error(err.message);
     });
 
